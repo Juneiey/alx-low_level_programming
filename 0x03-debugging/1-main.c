21b48ef09ac5f15c56ec7469ae5e64ac0d8bd83f
Juneiey <juni.okardi@gmail.com>
2023-03-23 15:47:48 -0700

    like and sub

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
  * Return: 0
  */
 
-
 int main(void)
 
 {
@@ -16,7 +15,7 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*   putchar(i);*/
+	/*	 putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
