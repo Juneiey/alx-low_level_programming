68b62ea4a081e253ff069d06539b3cb02c32801a
Juneiey <juni.okardi@gmail.com>
2023-04-05 07:13:06 -0700

    she

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+int _putchar(char);
 void _puts_recursion(char *s);
-void _print_rev-recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
